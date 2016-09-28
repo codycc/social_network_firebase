c47b71e5245cd5bc212ff98f4b201f7fd6415a65
Cody Condon <codycondon50@gmail.com>
2016-09-28 11:38:13 -0400

    Initial Commit

---
+++ SocialNetworkCody/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialNetworkCody
+//
+//  Created by Cody Condon on 2016-09-28.
+//  Copyright © 2016 Cody Condon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
